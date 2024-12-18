6026b5a08878f88d47ea8703ba89bb9214a1c549
Abdul asik raja <asikraja2001@gmail.com>
2024-12-18 22:36:36 +0530

    Add project files.

---
+++ vote/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vote
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Eligible for vote");
+            }
+            else
+            {
+                Console.WriteLine("Not eligible for vote");
+            }
+        }
+    }
+}
